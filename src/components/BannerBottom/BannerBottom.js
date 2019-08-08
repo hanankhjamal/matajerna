9501b7d0294cd88a5a6409a816f59146919eca27
hanan jamal <hanan@merwas.com>
2019-08-08 04:11:31 +0300

    add some class and style to header screen

--- src/components/BannerBottom/BannerBottom.js
+++ src/components/BannerBottom/BannerBottom.js
@@ -1,18 +1,80 @@
 import React, { Component } from "react";
 
-import { Row, Col, Card, CardHeader, CardBody } from "reactstrap";
-
 import mobileBrand from "../../assets/images/banners/mobile-brand.png";
 import playstore from "../../assets/images/banners/playstore.png";
 import appstore from "../../assets/images/banners/appstore.png";
+import { Row, Col } from "reactstrap";
 class BannerBottom extends Component {
   state = {};
   render() {
     return (
-      <div class="container-fluid px-0 mt-4 mb-5">
-        <div class="row mx-0">
+      <Row
+        className="p-0 m-0"
+        style={{
+          backgroundImage: "linear-gradient(109deg, #39b1e8 0%, #0042c6 100%)",
+          width: "100%",
+          height: 165,
+          overflow: "hidden"
+        }}
+      >
+        <Col xs={2} md={4} className='d-flex  justify-content-center align-items-center'>
+          <div className="d-flex justify-content-center mt-2" style={{marginRight:100}}>
+            <img
+              src={mobileBrand}
+              className="img-responsive"
+            />
+          </div>
+        </Col>
+        <Col xs={4}  style={{marginTop:'3.8%'}}>
+          <h4
+            style={{
+              color: "#ffffff",
+              fontWeight: 400,
+              marginRight: 30,
+              fontFamily: "Noto Kufi Arabic",
+              textAlign: "right"
+            }}
+          >
+            {"حمل تطبيق متاجرنا"}
+          </h4>
+          <h4
+            style={{
+              color: "#ffffff",
+              fontWeight: 400,
+              marginRight: 30,
+              fontFamily: "Noto Kufi Arabic",
+              textAlign: "right"
+            }}
+          >
+            {" على موبايلك"}
+          </h4>
+        </Col>
+        <Col xs={6} md={4} className='d-flex  justify-content-start '  style={{marginTop:'3.8%'}}>
+          <div>
+            <img
+              src={playstore}
+              className="img-responsive"
+              style={{ width: 158, height: 53 }}
+            />
+            <img
+              src={appstore}
+              className="img-responsive"
+              style={{ width: 143, height: 39 }}
+            />
+          </div>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+export default BannerBottom;
+
+/**
+ * 
+ *   <div className="container-fluid px-0 mt-4 mb-5">
           <div
-            class="col-12 px-0 d-flex flex-row  justify-content-center"
+            className="col-12 px-0 d-flex flex-row align-items-center  justify-content-center"
             style={{
               backgroundImage:
                 "linear-gradient(109deg, #39b1e8 0%, #0042c6 100%)",
@@ -22,18 +84,14 @@ class BannerBottom extends Component {
             }}
           >
             <div className="d-flex justify-content-center mt-2">
-              <img src={mobileBrand} style={{ height: 270 }} />
+              <img src={mobileBrand} className='img-responsive' style={{ height: '150%' }} />
             </div>
             <div
-              className="d-flex  align-items-center"
-              style={{
-                width: "25%"
-              }}
+              className="d-flex align-items-center"
             >
-              <p
+              <h3
                 style={{
                   color: "#ffffff",
-                  fontSize: 32,
                   fontWeight: 400,
                   marginRight: 30,
                   fontFamily:'Noto Kufi Arabic',
@@ -41,22 +99,17 @@ class BannerBottom extends Component {
                 }}
               >
                 {"حمل تطبيق متاجرنا على موبايلك"}
-              </p>
+              </h3>
             </div>
             <div
               className="d-flex flex-row align-items-center"
               style={{ marginRight: 50 }}
             >
               <div>
-                <img src={playstore} style={{ width: 158, height: 53 }} />
-                <img src={appstore} style={{ width: 143, height: 39 }} />
+                <img src={playstore} className='img-responsive' style={{ width: 158, height: 53 }} />
+                <img src={appstore} className='img-responsive' style={{ width: 143, height: 39 }} />
               </div>
             </div>
           </div>
-        </div>
       </div>
-    );
-  }
-}
-
-export default BannerBottom;
+ */
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,149 +5,341 @@ class Header extends Component {
   state = {};
   render() {
     return (
-      <div
-        className="container mt-3 mb-3 header"
-        style={{
-          display: "flex",
-          flexDirection: "row",
-          backgroundColor: "#fff"
-        }}
-      >
-        <div>
-          <img
-            className="img-responsive"
-            src={require("../../assets/images/logo.png")}
-          />
-        </div>
+      <div className="container mb-2 mt-2" style={{ backgroundColor: '"#fff"' }}>
+        <Row>
+          <Col
+            xs={3}
+            md={2}
+            className="d-flex align-items-center justify-content-center mt-1 mb-1"
+          >
+            <img
+              className="img-responsive"
+              src={require("../../assets/images/logo.png")}
+              style={{ width: "90%" }}
+            />
+          </Col>
 
-        <div className="mr-4 ml-2 d-flex align-items-center">
-          <Input
-            className="search-input"
-            style={{
-              width: "30vw",
-              padding: "6%",
-              paddingRight: "3vw",
-              backgroundImage: `url(${search})`,
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "95% 50%",
-              color: "#bbbbbb",
-              fontSize: '14vm',
-              fontWeight: "400",
-              backgroundColor: "#efefef",
-              border: "none",
-              borderRadius: 28,
-              outline: "none"
-            }}
-            type="text"
-            placeholder="ابحث في متاجرنا"
-          />
-        </div>
+          <Col
+            xs={5}
+            md={3}
+            className="d-flex flex-row align-items-center justify-content-center"
+          >
+            <div
+              className="d-flex flex-row align-items-center"
+              style={{ backgroundColor: "#efefef", borderRadius: 28, padding:8 }}
+            >
+              <img
+                src={require("../../assets/images/search.png")}
+                alt="img"
+                className="img-responsive mr-2"
+                style={{ width: "8%", height: "8%" }}
+              />
+              <Input
+                className="search-input"
+                style={{
+                  width: "80%",
+                  backgroundColor: "#efefef",
+                  color: "#bbbbbb",
+                  fontFamily: "Noto Kufi Arabic",
+                  fontSize: 10,
+                  textAlign: "right",
+                  fontWeight: "400",
+                  backgroundColor: "#efefef",
+                  border: "none",
+                  outline: "none"
+                }}
+                type="text"
+                placeholder="ابحث في متاجرنا"
+              />
+            </div>
+          </Col>
 
-        <button
-          className="d-flex flex-row btn btn-default pr-4 pl-4 pt-1 pb-1"
-          style={{
-            backgroundImage:
-              " linear-gradient(270deg, #ffffff 0%, #ffffff 100%)",
-            boxShadow: "0 3px 6px rgba(136, 136, 136, 0.38)",
-            borderRadius: 30,
-            color: "#bbbbbb",
-            fontSize: 13,
-            fontWeight: "700"
-          }}
-        >
-          <img
-            src={require("../../assets/images/shop.png")}
-            alt="img"
-            className="img-responsive"
-          />
-          <p className="d-flex align-items-center mb-0 mt-1 mr-2 ">
-            {" متاجر "}
-          </p>
-        </button>
+          <Col
+            xs={3}
+            md={1}
+            className="d-flex align-items-center justify-content-center  mt-2 mb-2 p-1"
+            style={{
+              backgroundImage:
+                " linear-gradient(270deg, #ffffff 0%, #ffffff 100%)",
+              boxShadow: "0 3px 6px rgba(136, 136, 136, 0.38)",
+              borderRadius: 30
+            }}
+          >
+            <img
+              src={require("../../assets/images/shop.png")}
+              alt="img"
+              className="img-responsive"
+              style={{ width: "28%", height: "50%" }}
+            />
+            <h4
+              className="d-flex align-items-center mb-2 mt-2 mr-2 "
+              style={{
+                color: "#bbbbbb",
+                fontSize: 13,
+                fontWeight: "700",
+                fontFamily: "Noto Kufi Arabic"
+              }}
+            >
+              {" متاجر "}
+            </h4>
+          </Col>
 
-        <button
-          className="d-flex flex-row btn btn-default pr-4 pl-4 mr-2 pt-1 pb-1"
-          style={{
-            backgroundImage:
-              " linear-gradient(270deg, #ffffff 0%, #ffffff 100%)",
-            boxShadow: "0 3px 6px rgba(136, 136, 136, 0.38)",
-            borderRadius: 30,
-            color: "#bbbbbb",
-            fontSize: 13,
-            fontWeight: "700"
-          }}
-        >
-          <img
-            src={require("../../assets/images/buy.png")}
-            alt="img"
-            className="img-responsive"
-          />
-          <p className="d-flex align-items-center mb-0 mt-1 mr-1 ">
-            {" اشتري "}
-          </p>
-        </button>
-        <button
-          className="d-flex flex-row btn btn-default pr-4 pl-4 mr-2 pt-1 pb-1"
-          style={{
-            backgroundImage:
-              "linear-gradient(270deg, #ffbd43 0%, #ffa92f 100%)",
-            boxShadow: "0 3px 6px rgba(255, 185, 63, 0.38)",
-            borderRadius: 30,
-            color: "#fff",
-            fontSize: 14,
-            fontWeight: "700"
-          }}
-        >
-          <img
-            src={require("../../assets/images/camera.png")}
-            alt="img"
-            className="img-responsive"
-          />
-          <p className="d-flex align-items-center mb-0 mt-1 mr-1 ">
-            {" أضف عنوانك مجاناً "}
-          </p>
-        </button>
-        <div className="d-flex align-items-center mr-1">
-          <span
-            class="badge badge-notify"
+          <Col
+            xs={3}
+            md={1}
+            className="d-flex align-items-center justify-content-center  mt-2 mb-2 mr-2"
             style={{
-              backgroundColor: " #ff3f55",
-              position: "relative",
-              right: 1,
-              top: -12,
-              height: 10,
-              width: 10,
-              borderRadius: 50,
-              display: "inline-block"
+              backgroundImage:
+                " linear-gradient(270deg, #ffffff 0%, #ffffff 100%)",
+              boxShadow: "0 3px 6px rgba(136, 136, 136, 0.38)",
+              borderRadius: 30
             }}
-          />
-          <img
-            src={require("../../assets/images/chat.png")}
-            alt="chat"
-            className="img-responsive"
-          />
-        </div>
-        <div className="d-flex align-items-center mr-1">
-          <span
-            class="badge badge-notify"
+          >
+            <img
+              src={require("../../assets/images/buy.png")}
+              alt="img"
+              className="img-responsive"
+              style={{ width: "32%" }}
+            />
+            <h4
+              className="d-flex align-items-center mb-2 mt-2 mr-2 "
+              style={{
+                color: "#bbbbbb",
+                fontSize: 13,
+                fontWeight: "700",
+                fontFamily: "Noto Kufi Arabic"
+              }}
+            >
+              {" اشتري "}
+            </h4>
+          </Col>
+          <Col
+            xs={3}
+            md={2}
+            className="d-flex align-items-center justify-content-center mt-2 mb-2 mr-2"
             style={{
-              backgroundColor: " #ff3f55",
-              position: "relative",
-              right: 1,
-              top: -12,
-              height: 10,
-              width: 10,
-              borderRadius: 50,
-              display: "inline-block"
+              backgroundImage:
+                "linear-gradient(270deg, #ffbd43 0%, #ffa92f 100%)",
+              boxShadow: "0 3px 6px rgba(255, 185, 63, 0.38)",
+              borderRadius: 30,
+              color: "#fff"
             }}
-          />
-          <img
-            src={require("../../assets/images/notification.png")}
-            alt="notification"
-            className="img-responsive"
-          />
-        </div>
+          >
+            <img
+              src={require("../../assets/images/camera.png")}
+              alt="img"
+              className="img-responsive"
+              style={{
+                height: "50%"
+              }}
+            />
+            <h4
+              className="d-flex align-items-center mb-2 mt-2 mr-2 "
+              style={{
+                color: "#fff",
+                fontSize: 13,
+                fontWeight: "700",
+                fontFamily: "Noto Kufi Arabic"
+              }}
+            >
+              {" أضف عنوانك مجاناً "}
+            </h4>
+          </Col>
+
+          <Col md={2} xs={2} className="d-flex flex-row  mr-2 p-0">
+            <div className="d-flex align-items-center justify-content-start ">
+              <img
+                src={require("../../assets/images/big_pic.png")}
+                alt="notification"
+                style={{ width: 40, height: 40 }}
+                className="img-responsive rounded-circle"
+              />
+
+              <span
+                class="badge badge-notify"
+                style={{
+                  backgroundColor: " #ff3f55",
+                  position: "relative",
+                  right: 1,
+                  top: -12,
+                  height: 10,
+                  width: 10,
+                  borderRadius: 50,
+                  display: "inline-block"
+                }}
+              />
+              <img
+                src={require("../../assets/images/chat.png")}
+                alt="notification"
+                className="img-responsive"
+              />
+            </div>
+            <div className="d-flex align-items-center">
+              <span
+                class="badge badge-notify"
+                style={{
+                  backgroundColor: " #ff3f55",
+                  position: "relative",
+                  right: 1,
+                  top: -12,
+                  height: 10,
+                  width: 10,
+                  borderRadius: 50,
+                  display: "inline-block"
+                }}
+              />
+              <img
+                src={require("../../assets/images/notification.png")}
+                alt="notification"
+                className="img-responsive"
+              />
+            </div>
+          </Col>
+        </Row>
       </div>
+      // <div
+      //   className="container mt-3 mb-3 header"
+      //   style={{
+      //     display: "flex",
+      //     flexDirection: "row",
+      //     backgroundColor: "#fff"
+      //   }}
+      // >
+      //   <div>
+      //     <img
+      //       className="img-responsive"
+      //       src={require("../../assets/images/logo.png")}
+      //     />
+      //   </div>
+
+      //   <div className="mr-4 ml-2 d-flex align-items-center">
+      //     <Input
+      //       className="search-input"
+      //       style={{
+      //         width: "30vw",
+      //         padding: "6%",
+      //         paddingRight: "3vw",
+      //         backgroundImage: `url(${search})`,
+      //         backgroundRepeat: "no-repeat",
+      //         backgroundPosition: "95% 50%",
+      //         color: "#bbbbbb",
+      //         fontSize: '14vm',
+      //         fontWeight: "400",
+      //         backgroundColor: "#efefef",
+      //         border: "none",
+      //         borderRadius: 28,
+      //         outline: "none"
+      //       }}
+      //       type="text"
+      //       placeholder="ابحث في متاجرنا"
+      //     />
+      //   </div>
+
+      //   <button
+      //     className="d-flex flex-row btn btn-default pr-4 pl-4 pt-1 pb-1"
+      //     style={{
+      //       backgroundImage:
+      //         " linear-gradient(270deg, #ffffff 0%, #ffffff 100%)",
+      //       boxShadow: "0 3px 6px rgba(136, 136, 136, 0.38)",
+      //       borderRadius: 30,
+      //       color: "#bbbbbb",
+      //       fontSize: 13,
+      //       fontWeight: "700"
+      //     }}
+      //   >
+      //     <img
+      //       src={require("../../assets/images/shop.png")}
+      //       alt="img"
+      //       className="img-responsive"
+      //     />
+      //     <p className="d-flex align-items-center mb-0 mt-1 mr-2 ">
+      //       {" متاجر "}
+      //     </p>
+      //   </button>
+
+      //   <button
+      //     className="d-flex flex-row btn btn-default pr-4 pl-4 mr-2 pt-1 pb-1"
+      //     style={{
+      //       backgroundImage:
+      //         " linear-gradient(270deg, #ffffff 0%, #ffffff 100%)",
+      //       boxShadow: "0 3px 6px rgba(136, 136, 136, 0.38)",
+      //       borderRadius: 30,
+      //       color: "#bbbbbb",
+      //       fontSize: 13,
+      //       fontWeight: "700"
+      //     }}
+      //   >
+      //     <img
+      //       src={require("../../assets/images/buy.png")}
+      //       alt="img"
+      //       className="img-responsive"
+      //     />
+      //     <p className="d-flex align-items-center mb-0 mt-1 mr-1 ">
+      //       {" اشتري "}
+      //     </p>
+      //   </button>
+      //   <button
+      //     className="d-flex flex-row btn btn-default pr-4 pl-4 mr-2 pt-1 pb-1"
+      //     style={{
+      //       backgroundImage:
+      //         "linear-gradient(270deg, #ffbd43 0%, #ffa92f 100%)",
+      //       boxShadow: "0 3px 6px rgba(255, 185, 63, 0.38)",
+      //       borderRadius: 30,
+      //       color: "#fff",
+      //       fontSize: 14,
+      //       fontWeight: "700"
+      //     }}
+      //   >
+      //     <img
+      //       src={require("../../assets/images/camera.png")}
+      //       alt="img"
+      //       className="img-responsive"
+      //     />
+      //     <p className="d-flex align-items-center mb-0 mt-1 mr-1 ">
+      //       {" أضف عنوانك مجاناً "}
+      //     </p>
+      //   </button>
+      //   <div className="d-flex align-items-center mr-1">
+      //     <span
+      //       class="badge badge-notify"
+      //       style={{
+      //         backgroundColor: " #ff3f55",
+      //         position: "relative",
+      //         right: 1,
+      //         top: -12,
+      //         height: 10,
+      //         width: 10,
+      //         borderRadius: 50,
+      //         display: "inline-block"
+      //       }}
+      //     />
+      //     <img
+      //       src={require("../../assets/images/chat.png")}
+      //       alt="chat"
+      //       className="img-responsive"
+      //     />
+      //   </div>
+      //   <div className="d-flex align-items-center mr-1">
+      //     <span
+      //       class="badge badge-notify"
+      //       style={{
+      //         backgroundColor: " #ff3f55",
+      //         position: "relative",
+      //         right: 1,
+      //         top: -12,
+      //         height: 10,
+      //         width: 10,
+      //         borderRadius: 50,
+      //         display: "inline-block"
+      //       }}
+      //     />
+      //     <img
+      //       src={require("../../assets/images/notification.png")}
+      //       alt="notification"
+      //       className="img-responsive"
+      //     />
+      //   </div>
+      // </div>
     );
   }
 }
